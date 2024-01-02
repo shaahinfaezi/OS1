5b000d7c1bd17765dc9373bc9f7043ec9f2a3455
BioBn <f54568192@gmail.com>
2024-01-03 00:52:14 +0330

    first process

--- OS/OS/OS.c
+++ OS/OS/OS.c
@@ -10,14 +10,12 @@
 #include <string.h>
 #include <stdint.h>
 #include <pthread.h>
-#include <semaphore.h>
-
 
 
 struct arg {
 
-    char * path;
-    
+    char* path;
+
     int  id;
 
 };
@@ -26,16 +24,24 @@ pthread_t threads[100000];
 
 int threadCount = 0;
 
+int FileCount;
+
 pthread_mutex_t mutexQueue;
 
-void * search(void * arguments);
+pthread_mutex_t mutexCount;
+
+int pCount=0;
+
+int pipes[10000+1][2];
+
+void* search(void* arguments);
 
 
 void submitThread(struct arg* argsPass) {
 
     pthread_mutex_lock(&mutexQueue);
 
-    pthread_t *thread=(pthread_t *)malloc(sizeof(pthread_t));
+    pthread_t* thread = (pthread_t*)malloc(sizeof(pthread_t));
 
     pthread_create(thread, NULL, search, (void*)argsPass);
 
@@ -47,76 +53,76 @@ void submitThread(struct arg* argsPass) {
 }
 
 
+void* search(void* arguments) {
 
-void *search(void * arguments){
+    struct arg* args = (struct arg*)malloc(sizeof(struct arg));
 
-    struct arg * args=(struct arg *)malloc(sizeof(struct arg));
+    args = (struct arg*)arguments;
 
-    args=(struct arg *)arguments;
+    char* path = args->path;
 
-    char * path=args->path;
+    int id = args->id;
 
-    int id=args->id;
-    
-    DIR * dirFile;
+    DIR* dirFile;
 
-
-    if (!(dirFile = opendir(path))){
+    if (!(dirFile = opendir(path))) {
         return NULL;
     }
 
     struct dirent* hFile;
 
-    while (( hFile = readdir( dirFile )) != NULL ) 
-        {   
-        
-        if (strcmp(hFile->d_name, ".") == 0 || strcmp(hFile->d_name, "..") == 0)
-                continue;
-        
-        if(hFile->d_type==DT_DIR){
- 
+    while ((hFile = readdir(dirFile)) != NULL)
+    {
 
-                char filepath[strlen(path) + 1 + strlen(hFile->d_name) + 1];
+        if (strcmp(hFile->d_name, ".") == 0  strcmp(hFile->d_name, "..") == 0)
+            continue;
 
-                snprintf(filepath, sizeof filepath, "%s/%s", path, hFile->d_name);
+        if (hFile->d_type == DT_REG) {
 
-                char* temp=(char*)malloc(strlen(filepath)+1);
-
-                strcpy(temp,filepath);
-
-                printf("folder : %s\n",filepath);
-
-                struct arg* argsPass=(struct arg *)malloc(sizeof(struct arg));
-
-                argsPass->path = temp;
-
-                argsPass->id = id+1;
-
-                submitThread(argsPass);
-
+            printf("file : %s\n", hFile->d_name);
             
-            
+            pthread_mutex_lock(&mutexCount);
+
+            FileCount++;
+
+            printf("%d\n%d\n\n\n\n\n",pCount,FileCount);
+
+            pthread_mutex_unlock(&mutexCount);
+
+            write(pipes[pCount][1],&FileCount,sizeof(int));
+
         }
-        if (hFile->d_type==DT_REG){
+        if (hFile->d_type == DT_DIR) {
 
-                printf("file : %s\n",hFile->d_name);
+            char filepath[strlen(path) + 1 + strlen(hFile->d_name) + 1];
 
+            snprintf(filepath, sizeof filepath, "%s/%s", path, hFile->d_name);
 
-        } 
+            char* temp = (char*)malloc(strlen(filepath) + 1);
 
+            strcpy(temp, filepath);
 
-        
+            printf("folder : %s\n", filepath);
+
+            struct arg* argsPass = (struct arg*)malloc(sizeof(struct arg));
+
+            argsPass->path = temp;
+
+            argsPass->id = id + 1;
+
+            submitThread(argsPass);
+
+        }
+
 
     }
 
 
 
+
     closedir(dirFile);
 
-
 }
-
-
 
 
 int main()
@@ -124,29 +130,140 @@ int main()
 
     pthread_mutex_init(&mutexQueue, NULL);
 
+    pthread_mutex_init(&mutexCount, NULL);
 
-    char * p = strdup("/");
+    char* p = strdup("/home/hakir/Desktop/Main (2)/");
 
-    struct arg *args=(struct arg *)malloc(sizeof(struct arg));
+    int id=0;
+
+    struct arg* args = (struct arg*)malloc(sizeof(struct arg));
 
     args->path = p;
 
     args->id = 0;
 
-    submitThread(args);
+    DIR* dirFile;
 
-    for (int i = 0; i < threadCount; i++) {
-        if (pthread_join(threads[i], NULL) != 0) {
 
-            perror("Failed to join the thread");
+    if (!(dirFile = opendir(p))) {
+        return NULL;
+    }
+
+    struct dirent* hFile;
+
+    pid_t pids[10000];
+
+    pids[pCount]=getpid();
+
+    pipe(pipes[pCount]);
+
+    write(pipes[pCount][1],&FileCount,sizeof(int));
+
+
+    while ((hFile = readdir(dirFile)) != NULL)
+    {
+
+        if (strcmp(hFile->d_name, ".") == 0  strcmp(hFile->d_name, "..") == 0)
+            continue;
+
+        if (hFile->d_type == DT_REG) {
+
+            printf("file : %s\n", hFile->d_name);
+    
+        }
+        if (hFile->d_type == DT_DIR) {
+
+            char filepath[strlen(p) + 1 + strlen(hFile->d_name) + 1];
+
+            snprintf(filepath, sizeof filepath, "%s/%s", p, hFile->d_name);
+
+            printf("folder : %s\n", filepath);
+
+            if(pids[pCount]==0){
+            //child
+
+            char* temp = (char*)malloc(strlen(filepath) + 1);
+
+            strcpy(temp, filepath);
+
+            struct arg* argsPass = (struct arg*)malloc(sizeof(struct arg));
+
+            argsPass->path = temp;
+
+            argsPass->id = id + 1;
+                
+            submitThread(argsPass);
+
+
+
+                
+            }
+            else{
+                //parent
+                pCount++;
+
+                printf("\n\n\n\n p count = %d\n\n\n\n",pCount);
+
+                pipe(pipes[pCount]);
+
+                write(pipes[pCount][1],&FileCount,sizeof(int));
+
+                pids[pCount]=fork();
+
+
+            }
+
 
         }
+
     }
+
+
+    printf("\n\n\n\n p count = %d\n\n\n\n",pCount);
+
+
+    for(int i=0;i<pCount;i++){
+        wait(NULL);
+    }   
+
+
+
+
+     for(int i=0;i<threadCount;i++){
+    
+        if(threads[i]!=NULL){
+            pthread_join(threads[i], NULL);
+
+        }
+
+        pthread_exit(NULL);
+    }    
+
+    int temp=0;
+
+    for(int i=0;i<pCount;i++){
+
+
+        read(pipes[i][0],&FileCount,sizeof(int));
+
+        temp+=FileCount;
+
+        close(pipes[i][0]);
+        close(pipes[i][1]);
+
+    }
+        
+
+    printf("\n\n\n%d",temp);
+
+    
     pthread_mutex_destroy(&mutexQueue);
 
+    pthread_mutex_destroy(&mutexCount);
 
-    pthread_exit(NULL);
-    
+
+
+
 
     return 0;
 }
